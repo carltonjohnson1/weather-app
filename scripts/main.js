3874c64f46549b7036b8802e8fc6eca564cd829a
carltonjohnson1 <carlton@carltonjohnson.com>
2025-04-18 13:17:02 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
+const backgroundColor = document.querySelector(".card-background-base");
+
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
@@ -22,24 +24,47 @@ async function checkWeather(city) {
     
         if(data.weather[0].main == "Clouds"){
             weatherIcon.src = "images/clouds.png";
+
+            // backgroundColor.classList.remove("card-background-base");
+            backgroundColor.classList.add("card-background-clouds");
+            
            
         }
         
         else if(data.weather[0].main == "Clear"){
             weatherIcon.src = "images/clear.png";
+
+            // backgroundColor.classList.remove("card-background-base");
+            backgroundColor.classList.add("card-background-clear");
         }
         
         else if(data.weather[0].main == "Rain"){
             weatherIcon.src = "images/rain.png";
+
+            // backgroundColor.classList.remove("card-background-base");
+            backgroundColor.classList.add("card-background-rain");
         }
         
         else if(data.weather[0].main == "Drizzle"){
             weatherIcon.src = "images/drizzle.png";
+
+            // backgroundColor.classList.remove("card-background-base");
+            backgroundColor.classList.add("card-background-drizzle");
         }
         
         else if(data.weather[0].main == "Mist"){
             weatherIcon.src = "images/mist.png";
+
+            // backgroundColor.classList.remove("card-background-base");
+            backgroundColor.classList.add("card-background-mist");
         }
+
+        // else if(data.weather[0].main == "Snow"){
+        //     weatherIcon.src = "images/snow.png";
+
+        //     backgroundColor.classList.remove("card-background-base");
+        //     backgroundColor.classList.add("card-background-snow");
+        // }
     
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
